ee97f4776a0165ff59586f97d1adcfdc9734f7bd
Michael Digman <9694102+m-digman@users.noreply.github.com>
2022-01-20 15:45:34 +0000

    added upgrade type (major, minor or patch) and version difference

--- audit.py
+++ audit.py
@@ -7,7 +7,7 @@ import csv
 
 gh_config = github_config()
 
-csv_column = ["Repository", "Dependency", "From", "Status", "Severity", "CVSS", "Summary", "Affected Versions", "Patched Version", "Current Version", "Published", "Link", "Dismisser", "Reason", "Closed"]
+csv_column = ["Repository", "Dependency", "From", "Status", "Severity", "CVSS", "Summary", "Upgrade", "Diff", "Affected Versions", "Patched Version", "Current Version", "Published", "Link", "Dismisser", "Reason", "Closed"]
 
 gql_query = """
         query MyQuery ($repository: String!, $owner: String!) {
@@ -74,6 +74,27 @@ def get_date_from_utc_string(date_string):
         return datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%SZ").date()
 
 
+def get_semantic_versions(version):
+    versions = version.split(".")
+    return versions[0], versions[1], versions[2]
+
+
+def _get_upgrade_status(current_version, patched_version):
+    if current_version and patched_version:
+        current_version = current_version.replace("= ", "")
+        current_major, current_minor, current_patch = get_semantic_versions(current_version)
+        patched_major, patched_minor, patched_patch = get_semantic_versions(patched_version)
+
+        if patched_major > current_major:
+            return "Major", int(patched_major) - int(current_major)
+        elif patched_major == current_major and patched_minor > current_minor:
+            return "Minor", int(patched_minor) - int(current_minor)
+        elif patched_major == current_major and patched_minor == current_minor and patched_patch > current_patch:
+            return "Patch", int(patched_patch) - int(current_patch)
+        else:
+            return ""
+
+
 def extract_data(repo_name, data_node, data_rows):
     severity = data_node["securityVulnerability"]["severity"]
     cvss_score = data_node["securityVulnerability"]["advisory"]["cvss"]["score"]
@@ -96,7 +117,9 @@ def extract_data(repo_name, data_node, data_rows):
         dismisser = "{0} ({1})".format(data_node["dismisser"]["name"], data_node["dismisser"]["login"])
         date_closed = get_date_from_utc_string(data_node["dismissedAt"])
 
-    return [repo_name, dependency, manifest, status, severity, cvss_score, summary, affected_version, patched_version, current_version,
+    upgrade_type, upgrade_difference = _get_upgrade_status(current_version, patched_version)
+
+    return [repo_name, dependency, manifest, status, severity, cvss_score, summary, upgrade_type, upgrade_difference, affected_version, patched_version, current_version,
             date_published, link, dismisser, dismiss_reason, date_closed]
 
 
